b9ee9eed118577fdef8f6e0dad59ec3a1edb8077
shijianfang <shijianfang@xinhehui.com>
2016-12-06 18:35:02 +0800

    完善文件处理

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 
 ##关于日志
 移除logjs4,使用alinode和[pm2管理日志](https://wohugb.gitbooks.io/pm2/content/features/log.html)
+
+## To DO
+> * 热编译处理。
+> * Gulp工程化。
+> * 文档编写。
+> * 中间件编写。
+> * 路由设计。
--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var config = require('./app/configs/env.js');
 var view = require('./app/configs/view.js');
 var routes = require('./app/configs/router.js');
 
+
+var error = require('./app/middles/error.js');
+
 var app = express();
 
 // view engine setup
@@ -16,44 +19,16 @@ view(app);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'app/' + qNode.env + '/static/resources', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/' + qNode.env )));
 
 routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+// error
+error(app);
 
 module.exports = app;
